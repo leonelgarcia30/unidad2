83111f8a75a1448146e23c7bb0f89aef619fbefd
leonelgarcia30 <144144557+leonelgarcia30@users.noreply.github.com>
2023-09-05 08:46:32 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listatareas;
+
+/**
+ *
+ * @author leonel.garcia
+ */
+public class ListaTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1 = "Sacar el area de un triangulo";
+        String tarea2 = "Sacar el area de un cuadrado";
+        String tarea3 = "Sacar el area de un rectangulo";
+        String tarea4 = "Sacar el area de un circulo";
+        String tarea5= "Sacar la hipotenusa de un triangulo";
+        
+        System.out.println("La tarea 1: " + tarea1);
+        System.out.println("La tarea 2: " + tarea2);
+        System.out.println("La tarea 3: " + tarea3);
+        System.out.println("La tarea 4: " + tarea4);
+        System.out.println("La tarea 5: " + tarea5);
+    }
+    
+}
